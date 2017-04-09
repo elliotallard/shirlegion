b15523969960920de3e66156f69ce73be6823891
elliotallard <eallard@andrew.cmu.edu>
2017-04-08 20:34:48 -0400

    Delete credentials.py

--- credentials.py
+++
@@ -1,4 +0,0 @@
-import os
-os.environ['SPOTIPY_CLIENT_ID'] = 'f1995b2349c049bb8c19bb2e155a46b5'
-os.environ['SPOTIPY_CLIENT_SECRET'] = 'c49aa2c3ceb546bd9a34e1eb944f5394'
-os.environ['SPOTIPY_REDIRECT_URI'] = 'shirlegion://callback'
