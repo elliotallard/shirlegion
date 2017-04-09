12d7b2c402a5961c2ced699fd03caa07cbf95b1d
stephananth <sananth@andrew.cmu.edu>
2017-04-09 11:45:06 -0400

    Update 112 2048.py

--- 112 2048.py
+++ 112 2048.py
@@ -9,6 +9,7 @@ import time
 import random
 import math
 import os
+import copy
 
 class Struct(object): pass
 data = Struct()
@@ -42,27 +43,37 @@ def init(data):
     data.DARK_GRAY = (50, 50, 50)
     data.LIGHT_GRAY = (200, 200, 200)
     data.WHITE = (255, 255, 255)
-    data.colors = dictColorsByValue(data)
+    data.colors = {128: (246,222,5), 64: (0,0,102), 32: (0,0,255), 16: (71,159,247), 8: (153,204,255), 4: (193,193,219), 2: (224,224,224)}
 
     # Controller
     data.clock = pygame.time.Clock()
     data.mode = 'home'
-    data.isPlaying = True
+    data.isGameEnd = False
 
-    while (data.isPlaying):
+    data.endRow = -1
+    data.endCol = -1
+    data.endX = -1
+    data.endY = -1
+    data.dx = -1
+    data.dy = -1
+
+    while (not data.isGameEnd):
         pygame.display.update()
         data.time = data.clock.tick(50)
         for event in pygame.event.get():
             if (event.type == pygame.QUIT):
-                data.isPlaying = False
+                break
             elif (event.type == pygame.KEYDOWN):
                 keyPressed(data, event.key)
         data.screen.fill(data.WHITE)
         redrawAll(data)
         pygame.display.flip()
     drawGameOver(data)
+    pygame.time.delay(1000)
     pygame.quit()
     print('Bye!')
+
+
 
 # Model
 def newBoard(rows, cols):
@@ -108,7 +119,7 @@ def dictValuesByStaff(data):
 
 def dictStaffByValues(data):
     result = dict()
-    result[data.values[0]] = 'display42'
+    result[data.values[0]] = ''
     for value in data.values[1:]:
         result[value] = set()
     for staffPerson in data.valuesByStaff:
@@ -121,7 +132,9 @@ def generatePiece(data):
         for col in range(data.cols):
             if (data.board[row][col][0] == 0):
                 randomChoices += [(row, col)]
-    if (randomChoices == []): return 0
+    if (randomChoices == []):
+        data.isGameEnd = True
+        drawGameOver(data)
     place = random.choice(randomChoices)
     data.board[place[0]][place[1]][0] = random.choice(data.randomPieces)
 
@@ -140,13 +153,16 @@ def dictColorsByValue(data):
 
 # View
 def redrawAll(data):
-    if (data.mode == 'home'):
-        data.message = 'Press space to play!'
-        drawHomeMessage(data)
-    elif (data.mode == 'playGame'):
-        data.message = 'Not playGame42'
-        drawGame(data)
-        drawGameMessage(data)
+    if (data.isGameEnd):
+        drawGameOver(data)
+    else:
+        if (data.mode == 'home'):
+            data.message = 'Press space to play!'
+            drawHomeMessage(data)
+        elif (data.mode == 'playGame'):
+            data.message = 'Not playGame42'
+            drawGame(data)
+            drawGameMessage(data)
 
 def drawHomeMessage(data):
     text = data.font.render(data.message, True, data.BLUE)
@@ -160,7 +176,18 @@ def drawGameMessage(data):
     data.screen.blit(text, (data.width//2 - text.get_width()//2,
                             data.height//10 - text.get_height()*2))
 def drawGameOver(data):
-    text = data.font.render('Game Over!', True, data.BLACK)
+    text = data.font.render('Game Over!', True, data.WHITE)
+    data.screen.blit(text, (data.width//2 - text.get_width()//2,
+                            data.height - text.get_height()*2))
+    pygame.draw.rect(data.screen, data.colors[data.goal], (data.endX + data.dx*data.endCol,
+                 data.endY + data.dy*data.endRow, data.side, data.side))
+    text = data.bigFont.render('42', True, data.WHITE)
+    fortyTwoX = (data.endX + data.dx*data.endCol + data.side / 2 - text.get_width()//2)
+    fortyTwoY = (data.endY + data.dy*data.endRow + data.side / 2 - text.get_height()//2)
+    data.screen.blit(text, (fortyTwoX,fortyTwoY))
+
+def drawWinMessage(data):
+    text = data.font.render('You Won!', True, data.WHITE)
     data.screen.blit(text, (data.width//2 - text.get_width()//2,
                             data.height - text.get_height()*2))
 def drawGame(data):
@@ -168,31 +195,43 @@ def drawGame(data):
                                                          data.height))
     pygame.draw.rect(data.screen, data.WHITE, (data.padding, data.padding,
                      data.width-2*data.padding, data.height-2*data.padding))
-    for row in range(data.rows):
-        for col in range(data.cols):
-            x0 = y0 = data.padding + data.margin*2
-            dx = dy = data.margin*2 + data.side
-            pygame.draw.rect(data.screen, data.LIGHT_GRAY, (x0 + dx*col,
-                             y0 + dy*row, data.side, data.side))
-            if (data.board[row][col][0] != 0):
-                value = data.board[row][col][0]
-                pygame.draw.rect(data.screen, data.colors[value], (x0 + dx*col,
-                                 y0 + dy*row, data.side, data.side))
-                if (data.board[row][col][1] == None):
-                    staffMember = random.sample(data.staffByValues[value], 1)[0]
-                    data.board[row][col][1] = staffMember
-                else:
-                    staffMember = data.board[row][col][1]
-                path = (data.folder + os.sep +
-                        data.valuesByStaff[staffMember][0] + os.sep +
-                        staffMember + '.jpg')
-                img = pygame.image.load(path)
-                img = pygame.transform.scale(img, (data.side-4*data.margin,
-                                             data.side-4*data.margin))
-                data.screen.blit(img, (x0 + dx*col + 2*data.margin,
-                                 y0 + dy*row + 2*data.margin,
-                                 data.side - 2*data.margin,
-                                 data.side - 2*data.margin))
+    if (not data.isGameEnd):
+        try:
+            for row in range(data.rows):
+                for col in range(data.cols):
+                    x0 = y0 = data.padding + data.margin*2
+                    dx = dy = data.margin*2 + data.side
+                    pygame.draw.rect(data.screen, data.LIGHT_GRAY, (x0 + dx*col,
+                                     y0 + dy*row, data.side, data.side))
+                    if (data.board[row][col][0] != 0):
+                        value = data.board[row][col][0]
+                        if (value == data.goal):
+                            data.endRow = row
+                            data.endCol = col
+                            data.endX = x0
+                            data.endY = y0
+                            data.dx = dx
+                            data.dy = dy
+                            data.isGameEnd = True
+                        pygame.draw.rect(data.screen, data.colors[value], (x0 + dx*col,
+                                         y0 + dy*row, data.side, data.side))
+                        if (data.board[row][col][1] == None):
+                            staffMember = random.sample(data.staffByValues[value], 1)[0]
+                            data.board[row][col][1] = staffMember
+                        else:
+                            staffMember = data.board[row][col][1]
+                        path = (data.folder + os.sep +
+                                data.valuesByStaff[staffMember][0] + os.sep +
+                                staffMember + '.jpg')
+                        img = pygame.image.load(path)
+                        img = pygame.transform.scale(img, (data.side-4*data.margin,
+                                                     data.side-4*data.margin))
+                        data.screen.blit(img, (x0 + dx*col + 2*data.margin,
+                                         y0 + dy*row + 2*data.margin,
+                                         data.side - 2*data.margin,
+                                         data.side - 2*data.margin))
+        except:
+            drawGameOver(data)
 
 # Controller
 def keyPressed(data, key):
@@ -200,89 +239,22 @@ def keyPressed(data, key):
         if (key == pygame.K_SPACE):
             data.mode = 'playGame'
     elif (data.mode == 'playGame'):
+        boardBefore = copy.deepcopy(data.board)
         if (key == pygame.K_UP): makeMoveUp(data)
         elif (key == pygame.K_DOWN): makeMoveDown(data)
         elif (key == pygame.K_LEFT): makeMoveLeft(data)
         elif (key == pygame.K_RIGHT): makeMoveRight(data)
-
-# def moveUp(data):
-#     for col in range(data.cols):
-#         for row in range(data.rows):
-#             if (data.board[row][col][0] != 0):
-#                 moveUpRecursively(data, row, col, data.board[row][col])
-#     generatePiece(data)
-# 
-# def moveUpRecursively(data, row, col, value):
-#     if (row == 0):
-#         data.board[row][col] = value
-#     elif (data.board[row-1][col][0] == 0):
-#         data.board[row][col] = [0, None]
-#         moveUpRecursively(data, row-1, col, value)
-#     elif (data.board[row-1][col][0] == data.board[row][col][0]):
-#         data.board[row][col] = [0, None]
-#         moveUpRecursively(data, row-1, col, [value[0] * 2, None])
-#     else:
-#         data.board[row][col] = value
-# 
-# def moveDown(data):
-#     for col in range(data.cols-1, -1, -1):
-#         for row in range(data.rows-1, -1, -1):
-#             if (data.board[row][col][0] != 0):
-#                 moveDownRecursively(data, row, col, data.board[row][col])
-#     generatePiece(data)
-# 
-# def moveDownRecursively(data, row, col, value):
-#     if (row == (data.rows-1)):
-#         data.board[row][col] = value
-#     elif (data.board[row+1][col][0] == 0):
-#         data.board[row][col] = [0, None]
-#         moveDownRecursively(data, row+1, col, value)
-#     elif (data.board[row+1][col][0] == data.board[row][col][0]):
-#         data.board[row][col] = [0, None]
-#         moveDownRecursively(data, row+1, col, [value[0] * 2, None])
-#     else:
-#         data.board[row][col] = value
-# 
-# def moveLeft(data):
-#     for row in range(data.rows):
-#         for col in range(data.cols):
-#             if (data.board[row][col][0] != 0):
-#                 moveLeftRecursively(data, row, col, data.board[row][col])
-#     generatePiece(data)
-# 
-# def moveLeftRecursively(data, row, col, value):
-#     if (col == 0):
-#         data.board[row][col] = value
-#     elif (data.board[row][col-1][0] == 0):
-#         data.board[row][col] = [0, None]
-#         moveLeftRecursively(data, row, col-1, value)
-#     elif (data.board[row][col-1][0] == data.board[row][col][0]):
-#         data.board[row][col] = [0, None]
-#         moveLeftRecursively(data, row, col-1, [value[0] * 2, None])
-#     else:
-#         data.board[row][col] = value
-# 
-# def moveRight(data):
-#     for row in range(data.rows-1, -1, -1):
-#         for col in range(data.cols-1, -1, -1):
-#             if (data.board[row][col][0] != 0):
-#                 moveRightRecursively(data, row, col, data.board[row][col])
-#     generatePiece(data)
-# 
-# def moveRightRecursively(data, row, col, value):
-#     if (col == (data.cols-1)):
-#         data.board[row][col] = value
-#     elif (data.board[row][col+1][0] == 0):
-#         data.board[row][col] = [0, None]
-#         moveRightRecursively(data, row, col+1, value)
-#     elif (data.board[row][col+1][0] == data.board[row][col][0]):
-#         data.board[row][col] = [0, None]
-#         moveRightRecursively(data, row, col+1, [value[0] * 2, None])
-#     else:
-#         data.board[row][col] = value
-# 
-# init(data)
-
+        if (boardBefore != data.board):
+            generatePiece(data)
+    for row in range(data.rows):
+        for col in range(data.cols):
+            if (data.board[row][col][0] == data.goal):
+                data.isGameEnd = True
+                drawGameOver(data)
+            elif (data.board[row][col][0] == 0):
+                return
+    data.isGameEnd = True
+    drawGameOver(data)
 
 def makeMoveUp(data):
     board = data.board
@@ -305,7 +277,7 @@ def makeMoveUp(data):
                         board[newRow][col] = [value*2, None]
                         noComboList.append((newRow,col))
                         board[newRow+1][col]= [0, None]
-    generatePiece(data)
+
     
 def makeMoveDown(data):
     board= data.board
@@ -327,7 +299,7 @@ def makeMoveDown(data):
                         board[newRow][col] = [value * 2, None]
                         noComboList.append((newRow,col))
                         board[newRow-1][col] = [0, None]
-    generatePiece(data)
+
 
 def makeMoveLeft(data):
     board = data.board
@@ -350,7 +322,6 @@ def makeMoveLeft(data):
                         board[row][newCol] = [value*2, None]
                         noComboList.append((row, newCol))
                         board[row][newCol+1] = [0, None]
-    generatePiece(data)
     
 def makeMoveRight(data):
     board = data.board
@@ -372,8 +343,5 @@ def makeMoveRight(data):
                         board[row][newCol] = [value*2, None]
                         noComboList.append((row, newCol))
                         board[row][newCol-1] = [0, None]
-    generatePiece(data)
-    
 
-    
 init(data)
